92b55f50f15ea8f953f867ed3c13d70625868ab5
phirmware <chibuzor.ojukwu@gmail.com>
2020-03-18 23:30:50 +0100

    my img

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/views'));
 
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
